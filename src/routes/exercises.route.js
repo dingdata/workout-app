723d6dc9ce91746e0b127984719063c329649d36
dingdata <dingbro@gmail.com>
2021-09-10 14:33:48 +0800

    Add Random function to retrieve a random exercise

--- src/routes/exercises.route.js
+++ src/routes/exercises.route.js
@@ -6,6 +6,23 @@ router.get("/", async (req, res) => {
   try {
     console.log("Exercise Route Get all");
     const exercises = await db.Exercise.findOne();
+
+    res.json(exercises);
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+});
+
+router.get("/random", async (req, res) => {
+  try {
+    console.log("Exercise Route Get random");
+
+    const exercises = await db.Exercise.findOne({
+      order: db.sequelize.random(),
+    });
+    console.log(exercises);
+
     res.json(exercises);
   } catch (err) {
     console.error(err);
