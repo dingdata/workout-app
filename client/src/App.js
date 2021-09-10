bdd882d7ba47173e776d3ca8207223ccdbd81784
fryfritter <ryan.devqr@gmail.com>
2021-09-10 14:28:18 +0800

    add basic navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Nav, Navbar } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Link } from "react-router-dom";
+import Login from "./components/login";
+import Signup from "./components/signup";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <header />
+        <Navbar bg="dark" variant="dark">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/login" data-testid="ti_nav_signup">
+              Login
+            </Nav.Link>
+            <Nav.Link as={Link} to="/signup" data-testid="ti_nav_signup">
+              Sign Up
+            </Nav.Link>
+          </Nav>
+        </Navbar>
+        <br />
+        <Switch>
+          <Route exact path="/signup" component={Signup} />
+          <Route exact path="/login" component={Login} />
+
+          <Route exact path="/" component={Login} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ client/src/components/login.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div>
+      <div>Hello from Login</div>
+    </div>
+  );
+};
+
+export default Login;
