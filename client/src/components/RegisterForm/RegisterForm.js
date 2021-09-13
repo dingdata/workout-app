40d90395d9cfad93234eb2e07367067337af4aa6
qdchongMom <chong_qi_da@mom.gov.sg>
2021-09-13 14:29:35 +0800

    add signup logic

--- client/src/components/RegisterForm/RegisterForm.js
+++ client/src/components/RegisterForm/RegisterForm.js
@@ -1,17 +1,40 @@
 import React from "react";
 
 import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
 import "./RegisterForm.scss";
+import * as Yup from "yup";
+import axios from "axios";
 //import "normalize.css";
 
 export default function RegisterForm() {
+  const validationSchema = Yup.object().shape({
+    firstName: Yup.string().required("First Name is required"),
+    lastName: Yup.string().required("Last Name is required"),
+    email: Yup.string().required("Email is required").email("Email is invalid"),
+    password: Yup.string()
+      .min(8, "Password must be at least 8 characters")
+      .required("Password is required"),
+    confirmPassword: Yup.string()
+      .oneOf([Yup.ref("password"), null], "Passwords must match")
+      .required("Confirm Password is required"),
+  });
+  const formOptions = { resolver: yupResolver(validationSchema) };
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
-  } = useForm();
-  const onSubmit = (data) => console.log(data);
+  } = useForm(formOptions);
+  const onSubmit = async (data) => {
+    //console.log(data);
+    const res = await axios.post("http://localhost:4000/api/users", {
+      firstName: data.firstName,
+      lastName: data.lastName,
+      emailAddress: data.email,
+      password: data.password,
+    });
+    console.log(res);
+  };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} className="register-form">
@@ -19,37 +42,61 @@ export default function RegisterForm() {
       <div classname="name-group">
         <div className="form-group">
           <input
-            {...register("firstName", { required: true })}
+            name="firstName"
+            type="text"
+            {...register("firstName")}
             placeholder="First Name"
           />
+          {errors.firstName && (
+            <span className="error-display">{errors.firstName.message}</span>
+          )}
         </div>
         <div className="form-group">
           <input
-            {...register("lastName", { required: true })}
+            name="lastName"
+            type="text"
+            {...register("lastName")}
             placeholder="Last Name"
           />
+          {errors.lastName && (
+            <span className="error-display">{errors.lastName.message}</span>
+          )}
         </div>
       </div>
       <div className="form-group">
         <input
-          name="emailAddress"
+          name="email"
+          type="text"
+          {...register("email")}
           placeholder="Email Address"
-          //   ref={register({ required: true })}
         />
+        {errors.email && (
+          <span className="error-display">{errors.email.message}</span>
+        )}
       </div>
       <div className="form-group">
         <input
-          name="createPassword"
           placeholder="Create Password"
-          //   ref={register({ required: true })}
+          name="password"
+          type="password"
+          {...register("password")}
         />
+        {errors.password && (
+          <span className="error-display">{errors.password.message}</span>
+        )}
       </div>
       <div className="form-group">
         <input
-          name="createPassword"
           placeholder="Confirm Password"
-          //   ref={register({ required: true })}
+          name="confirmPassword"
+          type="password"
+          {...register("confirmPassword")}
         />
+        {errors.confirmPassword && (
+          <span className="error-display">
+            {errors.confirmPassword.message}
+          </span>
+        )}
       </div>
       <button type="submit" className="form-submit-button">
         Register
