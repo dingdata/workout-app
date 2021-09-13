37e7062a54692fd152c9892ddd4b8c151571ac39
CP Tan <spin555@hotmail.com>
2021-09-13 10:41:05 +0800

    update images and wordings to carousel in client home js and css

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -10,11 +10,11 @@ const Home = () => {
         <Carousel.Item>
           <img
             className="d-block w-100"
-            src="./images/carousel_plank.jpg"
-            alt="c_plank"
+            src="./images/carousel_gal.jpg"
+            alt="c_gal"
           />
           <Carousel.Caption>
-            <div>Take it easy and enjoy the workout</div>
+            <div>Smile and breathe during the workout</div>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
@@ -24,18 +24,27 @@ const Home = () => {
             alt="c_parentkid"
           />
           <Carousel.Caption>
-            <div>Exercise is one the best family bonding activity</div>
+            <div>Exercise is the best family bonding activity</div>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img
             className="d-block w-100"
-            href="https://www.cheekiemonkie.net/2021/02/off-the-beaten-track-hiking-trails-singapore-for-families.html" //NOT WORKING
             src="./images/carousel_couple.jpg"
             alt="c_couple"
           />
           <Carousel.Caption>
-            <div>Workout and get fit together</div>
+            <div>Get fit together with your partner or buddy</div>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="./images/carousel_guy.jpg"
+            alt="c_guy"
+          />
+          <Carousel.Caption>
+            <div>Exercise is the best for self care</div>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
