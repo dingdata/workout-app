b0039bb690abe858bd591d829b4d5f6a416649ed
Sabrina Sulong <sabrina.sulong@thoughtworks.com>
2021-09-14 17:12:00 +0800

    Organise users route test cases
    Also moved truncate to afterAll hook to clean up test db

--- __test__/users/users.route.test.js
+++ __test__/users/users.route.test.js
@@ -10,41 +10,40 @@ app.use("/users", usersRouter);
 
 const jwt = require("jsonwebtoken");
 
-describe("POST /users", () => {
+describe("Users", () => {
   beforeAll(async () => {
     await db.sequelize.sync({ force: true });
     jest.spyOn(jwt, "sign");
   });
 
-  beforeEach(async () => {
-    await db.User.truncate({ cascade: true });
-  });
-
   afterAll(async () => {
+    await db.User.truncate({ cascade: true });
     await db.sequelize.close();
   });
 
-  it("should create a new user", async () => {
-    const newUser = {
-      firstName: "ah kow",
-      lastName: "tan",
-      emailAddress: "ah_kow@test.com",
-      password: "12345678",
-    };
+  describe("POST /users", () => {
+    it("should create a new user", async () => {
+      const newUser = {
+        firstName: "ah kow",
+        lastName: "tan",
+        emailAddress: "ah_kow@test.com",
+        password: "12345678",
+      };
 
-    const expectedUser = {
-      firstName: "ah kow",
-      lastName: "tan",
-      emailAddress: "ah_kow@test.com",
-    };
+      const expectedUser = {
+        firstName: "ah kow",
+        lastName: "tan",
+        emailAddress: "ah_kow@test.com",
+      };
 
-    const { body: actualUser, header } = await request(app)
-      .post("/users")
-      .send(newUser)
-      .expect(201);
+      const { body: actualUser, header } = await request(app)
+        .post("/users")
+        .send(newUser)
+        .expect(201);
 
-    expect(actualUser).toMatchObject(expectedUser);
-    expect(jwt.sign).toHaveBeenCalled();
-    expect(header["set-cookie"]).not.toBeUndefined();
+      expect(actualUser).toMatchObject(expectedUser);
+      expect(jwt.sign).toHaveBeenCalled();
+      expect(header["set-cookie"]).not.toBeUndefined();
+    });
   });
 });
