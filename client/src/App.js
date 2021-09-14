9877923d769d1a7fe6baa986897008ed618badf6
Sim Jiajin <backup_sjj@hotmail.com>
2021-09-14 16:15:35 +0800

    add router protect logic.  default is logged in in useCurrentUserHook.js

--- client/src/App.js
+++ client/src/App.js
@@ -14,64 +14,96 @@ import LoggedInUser from "./components/LoggedInUser";
 import DisplayRandom from "./components/Exercises/DisplayRandom";
 import DisplayWorkoutItem from "./components/Exercises/DisplayWorkoutItem";
 
+import { UserContext } from "./context/user";
+import { useCurrentUserHook } from "./hooks/useCurrentUserHook";
+
 function App() {
+  //currentUserHook for updating/retrieving login user
+  const { currentUser, setCurrentUser } = useCurrentUserHook();
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar bg="light" variant="light" defaultActiveKey="/random">
-          <Nav.Link as={Link} to="/Home">
-            <Image
-              src={process.env.PUBLIC_URL + "/images/logo.png"}
-              width="50px"
-            />
-          </Nav.Link>
-          <Nav>
-            <Nav.Link
-              as={Link}
-              to="/howItWorks"
-              data-testid="ti_nav_howitworks"
-              className="nav-link-text"
-              eventKey="link-1"
-            >
-              How it works
+      <UserContext.Provider value={currentUser}>
+        <BrowserRouter>
+          <Navbar bg="light" variant="light" defaultActiveKey="/random">
+            <Nav.Link as={Link} to="/Home">
+              <Image
+                src={process.env.PUBLIC_URL + "/images/logo.png"}
+                width="50px"
+              />
             </Nav.Link>
-            <Nav.Link as={Link} to="/allWorkouts" eventKey="link-2">
-              All Workouts
-            </Nav.Link>
-            <div className="nav-link-right-aligned">
-              <Link
+            <Nav>
+              <Nav.Link
                 as={Link}
-                to="/login"
-                eventKey="link-3"
-                style={{ textDecoration: "none" }}
+                to="/howItWorks"
+                data-testid="ti_nav_howitworks"
+                className="nav-link-text"
+                eventKey="link-1"
               >
-                <div className="button__primary button__link">Login</div>
-              </Link>
-              <Link
-                as={Link}
-                to="/signup"
-                eventKey="link-4"
-                style={{ textDecoration: "none" }}
-              >
-                <div className="button__secondary button__link">Sign Up</div>
-              </Link>
-            </div>
-          </Nav>
-        </Navbar>
+                How it works
+              </Nav.Link>
+              {currentUser && (
+                <Nav.Link as={Link} to="/allWorkouts" eventKey="link-2">
+                  All Workouts
+                </Nav.Link>
+              )}
 
-        <Switch>
-          <Route exact path="/signup" component={Signup} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/allWorkouts" component={DisplayAll} />
-          <Route exact path="/random" component={DisplayRandom} />
-          <Route exact path="/workoutItem" component={DisplayWorkoutItem} />
-          <Route exact path="/Home" component={Home} />
-          <Route exact path="/LoggedInMain" component={LoggedInUser} />
+              {currentUser && (
+                <div className="nav-link-right-aligned">
+                  <h2>Hi, {currentUser.username}!</h2>
+                </div>
+              )}
+              {!currentUser && (
+                <div className="nav-link-right-aligned">
+                  {" "}
+                  <Link
+                    as={Link}
+                    to="/login"
+                    eventKey="link-3"
+                    style={{ textDecoration: "none" }}
+                  >
+                    <div className="button__primary button__link">Login</div>
+                  </Link>
+                  <Link
+                    as={Link}
+                    to="/signup"
+                    eventKey="link-4"
+                    style={{ textDecoration: "none" }}
+                  >
+                    <div className="button__secondary button__link">
+                      Sign Up
+                    </div>
+                  </Link>
+                </div>
+              )}
+            </Nav>
+          </Navbar>
 
-          <Route exact path="/howItWorks" component={HowItWorks} />
-          <Route exact path="/" component={Home} />
-        </Switch>
-      </BrowserRouter>
+          <Switch>
+            <Route exact path="/Home" component={Home} />
+            <Route exact path="/howItWorks" component={HowItWorks} />
+            <Route exact path="/" component={Home} />
+            {currentUser && (
+              <div>
+                {" "}
+                <Route exact path="/allWorkouts" component={DisplayAll} />
+                <Route
+                  exact
+                  path="/workoutItem"
+                  component={DisplayWorkoutItem}
+                />
+                <Route exact path="/LoggedInMain" component={LoggedInUser} />
+              </div>
+            )}
+            {!currentUser && (
+              <div>
+                <Route exact path="/signup" component={Signup} />
+                <Route exact path="/login" component={Login} />{" "}
+                <Route exact path="/random" component={DisplayRandom} />
+              </div>
+            )}
+          </Switch>
+        </BrowserRouter>
+      </UserContext.Provider>
     </div>
   );
 }
