a64d631f7969f5edc5823a09780006f4c0890e9e
qdchongMom <chong_qi_da@mom.gov.sg>
2021-09-14 10:30:58 +0800

    add db teardown for user test

--- __test__/users/users.route.test.js
+++ __test__/users/users.route.test.js
@@ -1,6 +1,7 @@
 const request = require("supertest");
 const express = require("express");
 const app = express();
+const db = require("../../db/models/index");
 app.use(express.json());
 
 const usersRouter = require("../../src/routes/users.route");
@@ -8,6 +9,18 @@ const usersRouter = require("../../src/routes/users.route");
 app.use("/users", usersRouter);
 
 describe("POST /users", () => {
+  beforeAll(async () => {
+    await db.sequelize.sync({ force: true });
+  });
+
+  beforeEach(async () => {
+    await db.User.truncate({ cascade: true });
+  });
+
+  afterAll(async () => {
+    await db.sequelize.close();
+  });
+
   it("should create a new user", async () => {
     const newUser = {
       firstName: "ah kow",
