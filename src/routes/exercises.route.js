c5b157329f2d21eeacef769ba03ffe7bb3c33f6d
Sabrina Sulong <sabrina.sulong@thoughtworks.com>
2021-09-14 16:27:08 +0800

    Clean up code
    - remove console.logs
    - remove .gitkeep(s)
    - remove incomplete test (breaks npm test)

--- src/routes/exercises.route.js
+++ src/routes/exercises.route.js
@@ -4,38 +4,23 @@ const db = require("../../db/models/index");
 
 router.get("/", async (req, res, next) => {
   try {
-    console.log("Exercise Route Get all");
     const exercises = await db.Exercise.findAll();
     res.json(exercises);
   } catch (err) {
-    console.error(err);
     next(err);
   }
 });
 
 router.get("/random", async (req, res, next) => {
   try {
-    console.log("Exercise Route Get random");
-
-    const exercises = await db.Exercise.findOne({
+    const exercise = await db.Exercise.findOne({
       order: db.sequelize.random(),
     });
-    console.log(exercises);
-    res.json(exercises);
+
+    res.json(exercise);
   } catch (err) {
-    console.error(err);
     next(err);
   }
 });
 
 module.exports = router;
-
-// router.use((err, req, res, next) => {
-//     if (err.message === "Invalid ID") {
-//       console.log(err.message);
-//       res.sendStatus(err.status);
-//     } else {
-//       console.log(err.message);
-//       next(err);
-//     }
-//   });
