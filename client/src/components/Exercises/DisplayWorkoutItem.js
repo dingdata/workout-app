d7eae3d2fce904a14d16f07ec28bf04a4a6625df
dingdata <dingbro@gmail.com>
2021-09-14 14:36:28 +0800

    Added Shadows to buttons and added back and completed buttons in video page

--- client/src/components/Exercises/DisplayWorkoutItem.js
+++ client/src/components/Exercises/DisplayWorkoutItem.js
@@ -1,10 +1,39 @@
 import React from "react";
 import YoutubeIframe from "./YoutubeIframe";
+import { useHistory } from "react-router-dom";
 
 const DisplayWorkoutItem = (props) => {
+  const history = useHistory();
+
+  const backClickHandler = () => {
+    let path = `allWorkouts`;
+    history.push(path);
+  };
+
+  const completedClickHandler = () => {
+    let path = `howItWorks`;
+    history.push(path);
+  };
+
   return (
     <div className="workout-container">
       <YoutubeIframe tag={props.location.tag} />
+      <div>
+        <div className="button-container">
+          <button
+            className="button__primary button__link"
+            onClick={backClickHandler}
+          >
+            Back
+          </button>
+          <button
+            className="button__secondary button__link"
+            onClick={completedClickHandler}
+          >
+            Completed
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
