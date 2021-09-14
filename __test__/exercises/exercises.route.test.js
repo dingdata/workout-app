5c9de2740bba95817c64e947b269f183b749a71a
Sabrina Sulong <sabrina.sulong@thoughtworks.com>
2021-09-14 16:28:28 +0800

    Fix overall test suite, add tests for Exercise routes

--- __test__/exercises/exercises.route.test.js
+++ __test__/exercises/exercises.route.test.js
@@ -1,21 +1,59 @@
 const request = require("supertest");
 const express = require("express");
 const app = express();
+const db = require("../../db/models/index");
 app.use(express.json());
 
 const exercisesRouter = require("../../src/routes/exercises.route");
+app.use("/exercises", exercisesRouter);
 
-app.use("/exercises", exercisesRouter); // mounting the router
+describe("Exercises", () => {
+  beforeAll(async () => {
+    await db.sequelize.sync({ force: true });
 
-describe("GET /Random exercise", () => {
-  it("should return a random tag", async () => {
-    const exercise = await request(app).get("/exercises/random").expect(200);
-    console.log(exercise.body.tag);
-    expect(exercise.body.tag).toBeDefined();
+    const exercise1 = {
+      duration: 10,
+      intensity: "Moderate",
+      exerciseType: "Abs",
+      source: "youtube",
+      tag: "ZtnWJNkOy5w",
+      title: "Core and Legs Band Workout",
+    };
+    const exercise2 = {
+      duration: 10,
+      intensity: "Moderate",
+      exerciseType: "Abs",
+      source: "youtube",
+      tag: "iV8JGYFnOqk",
+      title: "Side Abs and Muffin Top",
+    };
+    await db.Exercise.create(exercise1);
+    await db.Exercise.create(exercise2);
   });
-  it("should return a all exercise", async () => {
-    const exercise = await request(app).get("/exercises").expect(200);
-    console.log(exercise.body);
-    expect(exercise.body.length).toBeGreaterThan(30);
+
+  afterAll(async () => {
+    await db.Exercise.truncate({ cascade: true });
+    await db.sequelize.close();
+  });
+
+  describe("GET /exercises/random", () => {
+    it("should return 1 random video", async () => {
+      const { body: exercise } = await request(app)
+        .get("/exercises/random")
+        .expect(200);
+
+      expect(exercise).not.toBeNull();
+      expect(exercise.tag).toBeDefined();
+    });
+  });
+
+  describe("GET /exercises", () => {
+    it("should return all exercises", async () => {
+      const { body: exercises } = await request(app)
+        .get("/exercises")
+        .expect(200);
+
+      expect(exercises.length).toBe(2);
+    });
   });
 });
