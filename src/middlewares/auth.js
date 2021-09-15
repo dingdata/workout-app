b7d3a4f674dae0c632d7012061871716f91be8b5
Sabrina Sulong <sabrina.sulong@thoughtworks.com>
2021-09-15 10:56:59 +0800

    Add auth middleware to check jwt token

---
+++ src/middlewares/auth.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+
+const auth = (req, res, next) => {
+  try {
+    if (!req.cookies.token) {
+      const err = new Error("You are not authorized");
+      next(err);
+    } else {
+      req.user = jwt.verify(req.cookies.token, process.env.JWT_SECRET_KEY);
+      next();
+    }
+  } catch (err) {
+    err.statusCode = 401;
+    next(err);
+  }
+};
+
+module.exports = {
+  auth,
+};
