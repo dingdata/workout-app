2de7546c1cf3b37e24f40933360fee06030ccbe6
fryfritter <ryan.devqr@gmail.com>
2021-09-15 10:34:43 +0800

    able to login via login button

--- client/src/components/UserLogin/login.js
+++ client/src/components/UserLogin/login.js
@@ -1,16 +1,41 @@
 import React from "react";
 import "./login.css";
-import { useForm } from "react-hook-form";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye } from "@fortawesome/free-solid-svg-icons";
+
+import { useForm } from "react-hook-form";
+import axios from "axios";
+import api from "../../constants/api";
+import { useHistory } from "react-router-dom";
+
 const eye = <FontAwesomeIcon icon={faEye} />;
 
 const Login = () => {
+  let history = useHistory();
+
   const { register, handleSubmit } = useForm();
-  const onSubmit = (data) => {
+
+  const onSubmit = async (data) => {
     console.log(data);
+    const res = await axios.post(api.users + "/login", {
+      emailAddress: data.emailAddress,
+      password: data.password,
+    });
+
+    //check if login successful
+    if (res.status === 200) {
+      //create jwt token
+      history.push("/allWorkouts");
+      //redirect to logged in screen
+    } else {
+      //error message
+      //how to bring back and display error on screen?
+    }
+
+    console.log(res);
   };
+
   return (
     <div class="container">
       <div className="background-image"></div>
@@ -19,6 +44,8 @@ const Login = () => {
           name="username"
           type="text"
           placeholder="Username"
+          {...register("emailAddress")}
+
           //ref={register({ required: "This is required." })}
         />
         <div className="pass-wrapper">
@@ -26,6 +53,8 @@ const Login = () => {
             placeholder="Password"
             name="password"
             type="password"
+            {...register("password")}
+
             //ref={register({ required: "This is required." })}
           />
           <i>{eye}</i>
