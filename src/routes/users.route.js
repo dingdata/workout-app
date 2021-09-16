420a5774669b38fe51cab6af8be7a85e9d063e83
qdchongMom <chong_qi_da@mom.gov.sg>
2021-09-16 09:17:15 +0800

    fix refresh on login

--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -82,7 +82,7 @@ router.post("/logout", async (req, res, next) => {
   }
 });
 
-router.get("/me", async (req, res, next) => {
+router.get("/me", auth, async (req, res, next) => {
   if (req.user) {
     let id = req.user.userId;
     const user = await db.User.findOne({
