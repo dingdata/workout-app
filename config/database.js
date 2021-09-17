b6fff4dbaf25104c19210444bec97de580098a27
qdchongMom <chong_qi_da@mom.gov.sg>
2021-09-17 17:25:46 +0800

    test circleci with diff host in database.js

--- config/database.js
+++ config/database.js
@@ -12,7 +12,7 @@ module.exports = {
     username: "postgres",
     password: null,
     database: process.env.TEST_DATABASE || "workout_app_test",
-    host: "127.0.0.1",
+    host: "127.0.0.10",
     dialect: "postgres",
   },
   production: {
